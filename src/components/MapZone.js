1966946b43c4517f7abdafd44e7dc0370008fea1
Christian <rva.christian91@gmail.com>
2018-06-26 19:23:36 -0700

    finally figured out delete functionality of dynamically created zones

--- src/components/MapZone.js
+++ src/components/MapZone.js
@@ -11,143 +11,191 @@ class MapZone extends PureComponent {
     this._handlePlaceMarker = this._handlePlaceMarker.bind(this);
     this._handleSetSelection = this._handleSetSelection.bind(this);
     this._getPolygonCoords = this._getPolygonCoords.bind(this);
+
+    this._handleDeleteZone = this._handleDeleteZone.bind(this);
+
+    this.selectedShape;
+
+    this.setState({
+      zones: [
+
+          {
+            name: "Zone 1"
+          }
+
+      ]
+    })
   }
 
   componentDidMount() {
-    console.log("google maps", google);
 
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        let map = new google.maps.Map(document.getElementById("map"), {
-          center: {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          },
-          zoom: 18
-        });
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+
+          // initializes the map
+          let map = new google.maps.Map(document.getElementById("map"), {
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            },
+            zoom: 18
+          });
+
+          // Construct the polygon
+          var aNewShapeCoords = [
+            new google.maps.LatLng(34.000559, -118.440392),
+            new google.maps.LatLng(34.000314, -118.439035),
+            new google.maps.LatLng(33.999532, -118.439791),
+            new google.maps.LatLng(33.999287, -118.440923),
+            new google.maps.LatLng(34.000096, -118.441224),
+            new google.maps.LatLng(34.000274, -118.440843)
+          ];
+          let aNewShape = new google.maps.Polygon({
+            paths: aNewShapeCoords,
+            draggable: false,
+            editable: true,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35
+          });
+
+          aNewShape.setMap(map);
+
+          google.maps.event.addListener(aNewShape, "click", function(event){
+
+              this._handleSetSelection(aNewShape);
+
+              console.log("NEW SHAPE", aNewShape)
+              this._getPolygonCoords(aNewShape);
+           
+          }.bind(this))
 
 
-         // Construct the polygon
-
-         var aNewShapeCoords = [
-         new google.maps.LatLng(34.000559,-118.440392),
-         new google.maps.LatLng(34.000314,-118.439035),
-         new google.maps.LatLng(33.999532,-118.439791),
-         new google.maps.LatLng(33.999287,-118.440923),
-         new google.maps.LatLng(34.000096,-118.441224),
-         new google.maps.LatLng(34.000274,-118.440843),
-     ];
-         let aNewShape = new google.maps.Polygon({
-             paths: aNewShapeCoords,
-             draggable: true,
-             editable: true,
-             strokeColor: '#FF0000',
-             strokeOpacity: 0.8,
-             strokeWeight: 2,
-             fillColor: '#FF0000',
-             fillOpacity: 0.35
-         });
-
-         aNewShape.setMap(map);
-    // google.maps.event.addListener(bermudaTriangle, "dragend", getPolygonCoords);
-    // google.maps.event.addListener(bermudaTriangle.getPath(), "insert_at", getPolygonCoords);
-    // google.maps.event.addListener(bermudaTriangle.getPath(), "remove_at", getPolygonCoords);
-    // google.maps.event.addListener(bermudaTriangle.getPath(), "set_at", getPolygonCoords);
-
-         var all_overlays = [];
+          // SETUP LISTENERS FOR ALL THESE SHAPES..
 
 
-           var selectedShape;
-           var drawingManager = new google.maps.drawing.DrawingManager({
-               drawingMode: google.maps.drawing.OverlayType.MARKER,
-               drawingControl: true,
-               drawingControlOptions: {
-                   position: google.maps.ControlPosition.TOP_CENTER,
-                   drawingModes: [
-                       google.maps.drawing.OverlayType.POLYGON,
-                   ]
-               },
+          
+          // END CONSTRUCT POLYGON
 
-               polygonOptions: {
-                   clickable: true,
-                   draggable: false,
-                   editable: false,
-                   fillColor: '#252525',
-                   fillOpacity: 0.4,
-               },
+        
+          var all_overlays = [];
 
-           });
+          var drawingManager = new google.maps.drawing.DrawingManager({
+            drawingMode: google.maps.drawing.OverlayType.POLYGON,
+            drawingControl: true,
+            drawingControlOptions: {
+              position: google.maps.ControlPosition.TOP_CENTER,
+              drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+            },
 
-           drawingManager.setMap(map);
+            polygonOptions: {
+              clickable: true,
+              draggable: false,
+              editable: false,
+              fillColor: "#252525",
+              fillOpacity: 0.4,
+
+            }
+          });
+
+          drawingManager.setMap(map);
+
+          google.maps.event.addListener(
+            drawingManager,
+            "overlaycomplete",
+            function(event) {
+
+              all_overlays.push(event);
+              drawingManager.setDrawingMode(null);
+              
+                var newShape = event.overlay;
+                newShape.type = event.type;
+
+                console.log("shape type", newShape)
+
+                
+                this._getPolygonCoords(newShape);
+
+                google.maps.event.addListener(
+                  newShape,
+                  "click",
+                  function() {
+                    this._handleSetSelection(newShape);
+                  }.bind(this)
+                );
+
+                this._handleSetSelection(newShape);
+              
+            }.bind(this)
+          );
+
+          all_overlays.push(event);
+          all_overlays.push()
 
 
-           google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
-
-               all_overlays.push(event);
-               console.log("OVERLAY EVENT", event.overlay)
-
-               if (event.type !== google.maps.drawing.OverlayType.MARKER) {
-                   drawingManager.setDrawingMode(null);
-                   //Write code to select the newly selected object.
-
-                   var newShape = event.overlay;
-                   newShape.type = event.type;
-
-                   console.log("EVENT COODINATES", newShape.getPath());
-
-                    this._getPolygonCoords(newShape)
-
-                   google.maps.event.addListener(newShape, 'click', function() {
-                     this._handleSetSelection(newShape);
-
-                   }.bind(this));
-
-                   this._handleSetSelection(newShape);
-               }
-           }.bind(this));
-
-
-
-            // var centerControlDiv = document.createElement('div');
-            // var centerControl = new CenterControl(centerControlDiv, map);
-            //
-            // centerControlDiv.index = 1;
-            // map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(centerControlDiv);
-
-
-      }.bind(this));
+        }.bind(this)
+      );
     }
   }
 
   _handlePlaceMarker(location, map) {
     let marker = new google.maps.Marker({
-        position: location,
-        map: map
+      position: location,
+      map: map
     });
   }
 
   _handleSetSelection(shape) {
-              //clearSelection();
-              var selectedShape = shape;
-              shape.setEditable(true);
-              // google.maps.event.addListener(selectedShape.getPath(), 'insert_at', getPolygonCoords(shape));
-              // google.maps.event.addListener(selectedShape.getPath(), 'set_at', getPolygonCoords(shape));
+
+    
+    console.log("shape is clicked", shape);
+    // this._handleClearSelection(shape);
+
+    if (this.selectedShape) {
+      this.selectedShape.setEditable(false) 
+    }
+
+    shape.setEditable(true);
+    this.selectedShape = shape;
+      
+  
+  }
+
+
+
+  _handleDeleteZone() {
+    if ( this.selectedShape ) {
+      this.selectedShape.setMap(null);
+      this.selectedShape = null;
+    }
+   
   }
 
   _getPolygonCoords = function(newShape) {
-               console.log("We are one");
-               var len = newShape.getPath().getLength();
-               console.log("new shape", len)
-               for (var i = 0; i < len; i++) {
-                   console.log("NEW SHAPE", newShape.getPath().getAt(i).toUrlValue(6));
-               }
-      };
+    console.log("NEW POLYGON SHAPE CREATED..");
+    var len = newShape.getPath().getLength();
+    console.log("new shape", len);
+    for (var i = 0; i < len; i++) {
+      console.log(
+        "NEW SHAPE",
+        newShape
+          .getPath()
+          .getAt(i)
+          .toUrlValue(6)
+      );
+    }
+  };
 
   render() {
     return (
       <div className="w-100p">
         GOOGLE MAPS
-        <div style={{height: '500px'}} id="map" />
+        <div style={{ height: "500px" }} id="map" />
+        <button onClick={()=> this._handleDeleteZone()}>Delete Zone</button>
+        
       </div>
     );
   }
